9e730a8bcca6b5b8524f4daaf7aa36a523e6c141
paraIncog <selgeandreas02@gmail.com>
2024-03-06 09:38:13 +0200

    Registering popup (FIX IT)

--- register.js
+++ register.js
@@ -1,2 +1,8 @@
 function register() {
+	const registerpopup = document.getElementById("registerpopup");
+	console.log(`Hello! Login had been clicked.`);
+	registerpopup.innerHTML = 
+		`
+			<div>Hello</div>
+		`;
 }
