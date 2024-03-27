950373c609d36ca79e9de48f4c1b86d4afeba8f5
paraIncog <selgeandreas02@gmail.com>
2024-03-27 09:51:20 +0200

    Reformat

--- friendsList.js
+++ friendsList.js
@@ -2,16 +2,16 @@ function friendsList(userId, isInFriendsList) {
 	if (isInPMPersonList) {
 		// Remove user from the private message list
 		removeFromFriendsList(userId);
-	  } else {
+	} else {
 		// Add user to the private message list
 		addToFriendsList(userId);
-	  }
+	}
 }
 
 function addToFriendsList(userId) {
 	// Logic to add the user to the private message list
-  }
+}
   
-  function removeFromFriendsList(userId) {
+function removeFromFriendsList(userId) {
 	// Logic to remove the user from the private message list
-  }
+}
